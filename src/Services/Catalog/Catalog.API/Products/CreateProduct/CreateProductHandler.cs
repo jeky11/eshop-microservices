115a90e79e39e3dc10696316c77cb75ad14d05d8
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-10-22 17:08:27 +0300

    add Marten

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,13 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         //create Product entity from command object
         var product = new Product
@@ -23,10 +20,10 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
         };
 
         //save to database
-        //session.Store(product);
-        // await session.SaveChangesAsync(cancellationToken);
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
         //return result
-        return Task.FromResult(new CreateProductResult(Guid.NewGuid()));
+        return new CreateProductResult(product.Id);
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -8,6 +8,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
 });
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);    
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
