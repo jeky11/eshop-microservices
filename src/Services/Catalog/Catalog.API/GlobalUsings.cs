d171f1b2760f784226e76abf3580679ad4432843
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-10-23 11:36:24 +0300

    GetProductById

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -4,4 +4,5 @@ global using MediatR;
 global using Marten;
 global using Marten.Pagination;
 global using BuildingBlocks.CQRS;
+global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
