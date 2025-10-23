081e3b0964ee3d26492d1ea38506ea33e10a8f06
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-10-23 16:09:24 +0300

    add CustomExceptionHandler

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,14 +14,15 @@ builder.Services.AddMediatR(config =>
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 
-builder.Services.AddMarten(opts =>
-{
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);    
-}).UseLightweightSessions();
+builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); }).UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
