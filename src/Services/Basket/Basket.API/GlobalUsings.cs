7e1fafcb4c9fc45b2c9a2f4674c1345b111fb447
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-11-03 18:24:43 +0300

    add basket endpoints

---
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿// Global using directives
+
+global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using FluentValidation;
+global using Mapster;
+global using MediatR;
