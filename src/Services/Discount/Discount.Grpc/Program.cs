5cdd163d66dabaca7fd26c993e391bacd5dcb4eb
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-11-05 13:07:40 +0300

    ability to generate client

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
@@ -12,5 +13,8 @@ app.MapGrpcService<GreeterService>();
 app.MapGet("/",
 	() =>
 		"Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+if (app.Environment.IsDevelopment())
+	app.MapGrpcReflectionService();
 
 app.Run();
