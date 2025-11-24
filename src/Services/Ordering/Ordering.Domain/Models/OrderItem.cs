d2a6095c563556852a893394dde9275545b1233b
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-11-24 18:25:55 +0300

    add domain models

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<OrderItemId>
+{
+	public OrderId OrderId { get; private set; } = default!;
+	public ProductId ProductId { get; private set; } = default!;
+	public int Quantity { get; private set; }
+	public decimal Price { get; private set; }
+
+	internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+	{
+		Id = OrderItemId.Of(Guid.NewGuid());
+		OrderId = orderId;
+		ProductId = productId;
+		Quantity = quantity;
+		Price = price;
+	}
+}
