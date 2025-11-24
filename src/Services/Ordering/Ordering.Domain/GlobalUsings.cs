f27fbc8c0821d97824684f7e8f76f89fe697a4a9
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-11-24 18:25:55 +0300

    add domain models

---
+++ src/Services/Ordering/Ordering.Domain/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using Ordering.Domain.Abstractions;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Events;
+global using Ordering.Domain.Exceptions;
+global using Ordering.Domain.ValueObjects;
