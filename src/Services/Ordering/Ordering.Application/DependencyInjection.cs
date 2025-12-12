a124fc3c20b0da2029a5e7233f13e5be82a6e63b
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-12-12 18:50:13 +0300

    event handlers

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using BuildingBlocks.Behaviors;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,8 +12,8 @@ public static class DependencyInjection
 		services.AddMediatR(config =>
 		{
 			config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-			//config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-			//config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+			config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+			config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 		});
 
 		return services;
