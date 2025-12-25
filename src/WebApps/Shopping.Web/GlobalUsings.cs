78e3e00e70e377057ff03e2eb9459bab7eb18e97
Dmitri Kholodov <dmitri.kholodov@gmail.com>
2025-12-25 15:08:12 +0300

    create Refit services

---
+++ src/WebApps/Shopping.Web/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿// Global using directives
+
+global using System.Net;
+global using Refit;
+global using Shopping.Web.Models.Basket;
+global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Ordering;
+global using Shopping.Web.Services;
--- src/WebApps/Shopping.Web/Program.cs
+++ src/WebApps/Shopping.Web/Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+var gatewayAddress = builder.Configuration["ApiSettings:GatewayAddress"] ?? throw new InvalidOperationException("GatewayAddress is not configured");
+builder.Services.AddRefitClient<ICatalogService>()
+	.ConfigureHttpClient(c => { c.BaseAddress = new Uri(gatewayAddress); });
+builder.Services.AddRefitClient<IBasketService>()
+	.ConfigureHttpClient(c => { c.BaseAddress = new Uri(gatewayAddress); });
+builder.Services.AddRefitClient<IOrderingService>()
+	.ConfigureHttpClient(c => { c.BaseAddress = new Uri(gatewayAddress); });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
